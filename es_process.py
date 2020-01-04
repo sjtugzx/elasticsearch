27bb784a2cf1a833f85429ec73e5fc57526bd227
Zhixin_Guo <troykuo@outlook.com>
2020-01-04 15:19:23 +0800

    create indexs

--- es_process.py
+++ es_process.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
 
     host = "10.10.10.10:9200"
     es = elasticsearch.Elasticsearch(host)
+    create_acm_index = es.indices.create(index='acm', ignore=400)
     print("es.ping(): ", es.ping())
+    print(create_acm_index)
     cnt = 0
     while cnt < 10:
         cnt += 1
@@ -20,7 +22,10 @@ if __name__ == '__main__':
                 context = f.read().replace('\n',' ')
         except FileNotFoundError:
             print("No such file or directory:"+'/Users/troykuo/Desktop/acm_papers/%s.txt' % cnt)
-        print(context)
+        # print(context)
         print('=' * 50 + '%s' % cnt + '=' * 50)
-        # data={'context':paper_context}
+        data={'context':paper_context}
         # operation.create_es_index(host='10.10.10.10:9200',index='acm', id=cnt, body=data)
+        # es.indices.delete(index='acm')
+        post_index=es.create(index='acm', doc_type='paper', id=cnt, body=data)
+        print(post_index)
